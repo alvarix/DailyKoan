02d8ef9406cbbcbfadee6cbaa5e83f0b180ab2fb
Alvar Sirlin <alvar@alvarsirlin.com>
2025-02-03 13:42:00 -0500

    Initial Commit

---
+++ DailyKoan/DailyKoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyKoanApp.swift
+//  DailyKoan
+//
+//  Created by Alvar Sirlin on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyKoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
